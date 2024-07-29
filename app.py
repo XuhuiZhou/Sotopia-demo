d9fecf8d402bc07f2428240b41f31bf86c61bc4c
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-07-28 17:51:40 -0700

    github token

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
+import subprocess
+import sys
+import time
 import streamlit as st
 os.environ["REDIS_OM_URL"] = st.secrets["REDIS_OM_URL"]
 print(os.environ['REDIS_OM_URL'])
 
 from sotopia.database import EpisodeLog
-from haicosystem.utils.render import render_for_humans # type: ignore
 from haicosystemDemo.hai_stream import streamlit_rendering
 
+try:
+  from haicosystem.utils.render import render_for_humans # type: ignore
+
+except ModuleNotFoundError as e:
+  subprocess.Popen([f'{sys.executable} -m pip install git+https://${st.secrets["GITHUB_TOKEN"]}@github.com/yourusername/yourrepo.git'], shell=True)
+  # wait for subprocess to install package before running your actual code below
+  time.sleep(90)
 
 st.title("HAICosystem Episode Rendering")
 # Text input for episode number
