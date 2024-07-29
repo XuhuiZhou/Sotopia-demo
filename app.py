f3c9deca0a838171eee9807459c83b2f3055b6cb
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-07-28 18:15:57 -0700

    update bug fix

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ import time
 import streamlit as st
 os.environ["REDIS_OM_URL"] = st.secrets["REDIS_OM_URL"]
 print(os.environ['REDIS_OM_URL'])
-
 from sotopia.database import EpisodeLog
-from haicosystemDemo.hai_stream import streamlit_rendering
 
 try:
-  from haicosystem.utils.render import render_for_humans # type: ignore
+  import haicosystem # type: ignore
 
 except ModuleNotFoundError as e:
-  subprocess.Popen([f'{sys.executable} -m pip install git+https://${st.secrets["GITHUB_TOKEN"]}@github.com/yourusername/yourrepo.git'], shell=True)
+  subprocess.Popen([f'{sys.executable} -m pip install git+https://{st.secrets["GITHUB_TOKEN"]}@github.com/XuhuiZhou/HAI-Cosys.git'], shell=True)
   # wait for subprocess to install package before running your actual code below
   time.sleep(90)
 
+from haicosystem.utils.render import render_for_humans # type: ignore
+from haicosystemDemo.hai_stream import streamlit_rendering
 st.title("HAICosystem Episode Rendering")
 # Text input for episode number
 episode_number = st.text_input("Enter episode number:", value="2")
